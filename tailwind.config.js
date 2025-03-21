a52fbc2e5dfcc3b61a44b77cd68f3c8877b2bfe1
Aria Aghakhani <48532756+Ariaaghakhani@users.noreply.github.com>
2025-03-21 17:08:54 +0330

    Project Start

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'dipo-black':'#141414',
+        'erie':"#212529",
+        'onyx':"#343A40",
+        'space':"#495057",
+        'french':"#ADB5BD",
+        'french-gray':"#CED4DA",
+        'platinum':"#DEE2E6",
+        'anti-flash':"#E9ECEF",
+        'salt':"#F8F9FA",
+      },
+      fontFamily:{
+        danaReg:'danaReg',
+        danaBold:'danaBold',
+        danaBlack:"danaBlack",
+        danaMed:"danaMed",
+        danaDemi:"danaDemi",
+        danaExtra:"danaExtra",
+        danaThin:"danaThin",
+      }
+    },
+  },
+  plugins: [],
+}
+
