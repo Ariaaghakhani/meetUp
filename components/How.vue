79a1428ff67ebb970661c508daaa74347a0175b5
Aria Aghakhani <48532756+Ariaaghakhani@users.noreply.github.com>
2025-03-27 12:10:16 +0330

    Commit 27 Mar 2025

--- components/How.vue
+++ components/How.vue
@@ -2,23 +2,70 @@
 <div class="w-full rounded-3xl bg-salt mt-40 py-40">
   <p class="text-center max-w-[500px] mx-auto text-prblue font-danaBold"> برگذار چطوری کار می‌کنه؟</p>
   <h2 class="text-center max-w-[500px] mx-auto text-4xl font-danaBold mt-4">خلق تجربه‌ی جذاب برای شما</h2>
-  <div class="flex w-full p-8 *:w-1/3 *:p-4 *:flex *:justify-center *:content-start *:flex-wrap">
+  <div class="hidden lg:flex w-full p-8 *:w-1/3 *:p-4 *:flex *:justify-center *:content-start *:flex-wrap">
     <div>
       <img src="@/assets/images/how1.png" alt="" class="w-[150px] h-[150px] mb-8">
       <div class=" how-box">
         <span class="font-danaBold rounded-full bg-prblue flex justify-center items-center text-white w-[40px] h-[40px]">۰۱</span>
       </div>
+      <h2 class="font-danaBold text-3xl max-w-[300px] mx-auto mt-8 text-center">
+        ایونت‌های اطرافت رو بررسی و جستجو کن
+      </h2>
     </div>
     <div>
       <img src="@/assets/images/how2.png" alt="" class="w-[150px] h-[150px] mb-8">
       <div class="how-box">
         <span class="font-danaBold rounded-full bg-prblue flex justify-center items-center text-white w-[40px] h-[40px]">۰۲</span>
       </div>
+      <h2 class="font-danaBold text-3xl max-w-[300px] mx-auto mt-8 text-center">
+        ایونتی که به علایقت نزدیکه رو رزرو کن
+      </h2>
     </div>
     <div>
       <img src="@/assets/images/how3.png" alt="" class="w-[150px] h-[150px] mb-8">
       <div class="w-full flex justify-center">
         <span class="font-danaBold rounded-full bg-prblue flex justify-center items-center text-white w-[40px] h-[40px] z-10">۰۳</span>
+      </div>
+      <h2 class="font-danaBold text-3xl max-w-[300px] mx-auto mt-8 text-center">
+        از تک تک لحظات انتخابت لذت ببر
+      </h2>
+    </div>
+  </div>
+  <div class="flex lg:hidden flex flex-wrap md:container mx-auto p-8 *:flex *:w-full *:mt-8">
+    <div class="flex items-start h-fit">
+      <img src="@/assets/images/how1.png" alt="" class="w-[150px] h-[150px] mb-8">
+      <div class="flex flex-wrap ms-8">
+        <div class="w-full mb-4 h-fit">
+          <span class="font-danaBold rounded-full bg-prblue flex justify-center items-center text-white w-[40px] h-[40px]">۰۱</span>
+        </div>
+        <h2 class="font-danaBold md:max-w-[400px] text-3xl text-start">
+          ایونت‌های اطرافت رو بررسی و جستجو کن
+        </h2>
+        <p class="text-space max-w-[400px] my-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam cumque eius enim laudantium maxime nisi non perferendis quaerat.</p>
+      </div>
+    </div>
+    <div class="flex items-start h-fit">
+      <img src="@/assets/images/how3.png" alt="" class="w-[150px] h-[150px] mb-8">
+      <div class="flex flex-wrap ms-8">
+        <div class="w-full mb-4 h-fit">
+          <span class="font-danaBold rounded-full bg-prblue flex justify-center items-center text-white w-[40px] h-[40px]">۰۲</span>
+        </div>
+        <h2 class="font-danaBold md:max-w-[400px] text-3xl text-start">
+          ایونتی که به علایقت نزدیکه رو رزرو کن
+        </h2>
+        <p class="text-space max-w-[400px] my-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam cumque eius enim laudantium maxime nisi non perferendis quaerat.</p>
+      </div>
+    </div>
+    <div class="flex items-start h-fit">
+      <img src="@/assets/images/how2.png" alt="" class="w-[150px] h-[150px] mb-8">
+      <div class="flex flex-wrap ms-8">
+        <div class="w-full mb-4 h-fit">
+          <span class="font-danaBold rounded-full bg-prblue flex justify-center items-center text-white w-[40px] h-[40px]">۰۳</span>
+        </div>
+        <h2 class="font-danaBold md:max-w-[400px] text-3xl text-start">
+          از تک تک لحظات انتخابت لذت ببر
+        </h2>
+        <p class="text-space max-w-[400px] my-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam cumque eius enim laudantium maxime nisi non perferendis quaerat.</p>
       </div>
     </div>
   </div>
